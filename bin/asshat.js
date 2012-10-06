f21694a0342d4a03cb5613e98fedd1891fec89c2
V Sreekanth <sreeix@gmail.com>
2012-10-06 14:38:12 +0530

    Version bump and adding asshat

---
+++ bin/asshat.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var app = require('commander');
+var hat = require('../lib/index.js');
+app
+  .version('0.0.1')
+  .option('-s, --stage [stage]', 'deploy to  a specific stage [production]', 'production')
+  .option('-n, --dryrun', 'Dry run the deployment')
+  .option('-v, --verbose', 'show lot of logs')
+  .parse(process.argv);
+  
+hat.exec(["rollback"], {stage: app.stage, dryrun: app.dryrun}, function(err, res){
+  console.log("finished");
+});
+
