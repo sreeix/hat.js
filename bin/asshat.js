0a33f9c412400fcca929d418f09cd34aee039fb7
V Sreekanth <sreeix@gmail.com>
2012-10-08 02:56:45 +0530

    Implementing rollback.
    Also simple mechanisms to parse responses from stdout and stderr
    in the responses from hosts.

--- bin/asshat.js
+++ bin/asshat.js
@@ -6,10 +6,14 @@ app
   .version('0.0.1')
   .option('-s, --stage [stage]', 'deploy to  a specific stage [production]', 'production')
   .option('-n, --dryrun', 'Dry run the deployment')
+  .option('-r, --revisions [revisions]', 'How many revisions to go backward', parseInt)
   .option('-v, --verbose', 'show lot of logs')
   .parse(process.argv);
   
-hat.exec(["rollback"], {stage: app.stage, dryrun: app.dryrun}, function(err, res){
+hat.exec(["rollback"], {stage: app.stage, dryrun: app.dryrun, revisions: app.revisions}, function(err, res){
+  if(err){
+    console.log(err);
+  }
   console.log("finished");
 });
 
