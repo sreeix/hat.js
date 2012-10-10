92c22f0f9b60a911034647e529340b98cdbac0c4
V Sreekanth <sreeix@gmail.com>
2012-10-11 04:11:51 +0530

    Simple Task listing and description.

--- bin/hat.js
+++ bin/hat.js
@@ -1,14 +1,28 @@
 #!/usr/bin/env node
 
 var app = require('commander');
+var _ = require('underscore');
 var hat = require('../lib/index.js');
+
+var showTaskTips = function(){
+  _.chain(hat)
+   .functions()
+   .filter(function filterPrivate (argument) { return argument[0] !== '_'})
+   .collect(function tips (item) {return {fun:item, tip: hat[item+"_tip"]}})
+   .compact()
+   .each(function(item){console.log(item.fun + " : "+ item.tip )});
+}
+
 app
   .version('0.0.3')
   .option('-s, --stage [stage]', 'deploy to  a specific stage [production]', 'production')
   .option('-n, --dryrun', 'Dry run the deployment')
+  .option('-t, --tasks', 'Show tasks available for execution.')
   .option('-v, --verbose', 'show lot of logs')
   .parse(process.argv);
-  
+  if(app.tasks){
+    process.exit(showTaskTips());
+  }
 hat.exec(app.args, {stage: app.stage, dryrun: app.dryrun}, function(err, res){
   console.log("finished");
 });
